8772dce46d92d7346240180304d5c94cd1702555
diegostafa <dstafa.dev@gmail.com>
2024-04-13 17:57:24 +0200

    ..

--- 4chan-auto-update-thread.user.js
+++ 4chan-auto-update-thread.user.js
@@ -3,27 +3,59 @@
 // @namespace   github.com/diegostafa/userscripts
 // @match       https://boards.4chan.org/*/thread/*
 // @match       https://boards.4channel.org/*/thread/*
-// @version     1
+// @version     2
 // @author      Diego <dstafa.dev@gmail.com> (github.com/diegostafa)
 // @description checkbox to automatically update the current thread every 5 seconds
 // @run-at      document-end
 // ==/UserScript==
 
-let autoUpdateCb = document.createElement('input');
-autoUpdateCb.type = 'checkbox';
-autoUpdateCb.id = 'autoUpdateThread';
-autoUpdateCb.name = 'autoUpdateThread';
+let thread = document.querySelector(".thread");
+let replies = thread.children.length;
 
-let autoUpdateLb = document.createElement('label');
-autoUpdateLb.htmlFor = 'autoUpdateThread';
-autoUpdateLb.appendChild(document.createTextNode('auto update thread'));
+let observer = new MutationObserver(() => {
+  let newReplies = thread.children.length;
+  if (replies == newReplies) return;
+  replies = newReplies;
 
-let botNavLinks = document.querySelector('.navLinks.navLinksBot');
-botNavLinks.appendChild(autoUpdateCb);
-botNavLinks.appendChild(autoUpdateLb);
+  document
+    .querySelector(".thread > :last-child")
+    .scrollIntoView({ behavior: "smooth", block: "start" });
+});
 
-let updateLink = document.querySelector('a[data-cmd="update"]');
-setInterval(() => {
-    if (autoUpdateCb.checked)
-        updateLink.click();
-}, 5000);
+const startObserving = () =>
+  observer.observe(thread, {
+    attributes: false,
+    childList: true,
+    subtree: true,
+  });
+const stopObserving = () => observer.disconnect();
+const toggleObserver = (cb) => () =>
+  cb.checked ? startObserving() : stopObserving();
+
+const buildUi = () => {
+  let cb = document.createElement("input");
+  cb.type = "checkbox";
+  cb.id = "autoUpdateThread";
+  cb.name = "autoUpdateThread";
+  cb.addEventListener("change", toggleObserver(cb));
+
+  let lb = document.createElement("label");
+  lb.htmlFor = "autoUpdateThread";
+  lb.appendChild(document.createTextNode("auto update thread"));
+
+  let botNavLinks = document.querySelector(".navLinks.navLinksBot");
+  botNavLinks.appendChild(cb);
+  botNavLinks.appendChild(lb);
+
+  return cb;
+};
+
+const autoUpdate = () => {
+  let cb = buildUi();
+  let updateBtn = document.querySelector('a[data-cmd="update"]');
+  setInterval(() => {
+    if (cb.checked) updateBtn.click();
+  }, 5000);
+};
+
+autoUpdate();
--- 4chan-sort-comments.user.js
+++ 4chan-sort-comments.user.js
@@ -3,118 +3,116 @@
 // @namespace   github.com/diegostafa/userscripts
 // @match       https://boards.4chan.org/*/thread/*
 // @match       https://boards.4channel.org/*/thread/*
-// @version     3
+// @version     4
 // @author      Diego <dstafa.dev@gmail.com> (github.com/diegostafa)
 // @description sort comments with differnt criterias (placed under the thread watcher)
 // @run-at      document-end
 // ==/UserScript==
 
+// --- regex
+
+const isVideoRegex = /.*(webm|gif)$/;
+const isUrlRegex = /.*:(\/\/|\?).*/gi;
+const isOpReplyRegex = /.* \(OP\)$/;
+
 const sortComments = (sortingRule) => {
-    let thread = document.querySelector(".thread");
-    let comments = [...thread.children];
+  let thread = document.querySelector(".thread");
+  let comments = [...thread.children];
 
-    comments.shift(); // ignore OP
-    comments.sort(sortingRule);
-    comments.forEach(comment => {
-        thread.appendChild(comment);
-    });
-};
-
-const urlRegex = /.*:(\/\/|\?).*/gi;
-
-const isImage = (file) => {
-    return !file.endsWith(".webm") && !file.endsWith(".gif");
+  comments.shift(); // ignore OP
+  comments.sort(sortingRule);
+  comments.forEach((comment) => {
+    thread.appendChild(comment);
+  });
 };
 
 const byRepliesDesc = (a, b) => {
-    const repliesA = a.querySelectorAll(".backlink > span").length;
-    const repliesB = b.querySelectorAll(".backlink > span").length;
-    return repliesB - repliesA;
+  const repliesA = a.querySelectorAll(".backlink > span").length;
+  const repliesB = b.querySelectorAll(".backlink > span").length;
+  return repliesB - repliesA;
 };
 
 const byTimeDesc = (a, b) => {
-    const timeA = a.querySelector(".dateTime").getAttribute("data-utc");
-    const timeB = b.querySelector(".dateTime").getAttribute("data-utc");
-    return timeA - timeB;
+  const timeA = a.querySelector(".dateTime").getAttribute("data-utc");
+  const timeB = b.querySelector(".dateTime").getAttribute("data-utc");
+  return timeA - timeB;
 };
 
 const byImagesFirst = (a, b) => {
-    const fileA = a.querySelector(".fileThumb");
-    const fileB = b.querySelector(".fileThumb");
-
-    if (fileB && isImage(fileB.getAttribute("href")))
-        return 1;
-    if (fileA && isImage(fileA.getAttribute("href")))
-        return -1;
-    return 0;
+  const fileA = a.querySelector(".fileThumb");
+  const fileB = b.querySelector(".fileThumb");
+  return (fileB && !isVideoRegex.test(fileB)) -
+    (fileA && !isVideoRegex.test(fileA));
 };
 
 const byVideosFirst = (a, b) => {
-    const fileA = a.querySelector(".fileThumb");
-    const fileB = b.querySelector(".fileThumb");
-
-    if (fileB && !isImage(fileB.getAttribute("href")))
-        return 1;
-    if (fileA && !isImage(fileA.getAttribute("href")))
-        return -1;
-    return 0;
+  const fileA = a.querySelector(".fileThumb");
+  const fileB = b.querySelector(".fileThumb");
+  return (fileB && isVideoRegex.test(fileB)) -
+    (fileA && isVideoRegex.test(fileA));
 };
 
 const byLinksFirst = (a, b) => {
-    const textA = a.querySelector(".postMessage").innerText;
-    const textB = b.querySelector(".postMessage").innerText;
-
-    return urlRegex.test(textB) - urlRegex.test(textA);
+  const textA = a.querySelector(".postMessage").innerText;
+  const textB = b.querySelector(".postMessage").innerText;
+  return isUrlRegex.test(textB) - isUrlRegex.test(textA);
 };
 
 const byDeadLinkFirst = (a, b) => {
-    const deadlinkA = a.querySelector(".deadlink");
-    const deadlinkB = b.querySelector(".deadlink");
-
-    if (deadlinkB)
-        return 1;
-    if (deadlinkA)
-        return -1;
-    return 0;
+  const deadlinkA = a.querySelector(".deadlink");
+  const deadlinkB = b.querySelector(".deadlink");
+  return deadlinkB - deadlinkA;
 };
 
-const createSortButton = (text, action) => {
-    let button = document.createElement("div");
-    button.addEventListener("click", action);
-    button.innerHTML = text;
-    button.style.backgroundColor = "#444444";
-    button.style.color = "white";
-    button.style.textAlign = "center";
-    button.style.border = "none";
-    button.style.padding = "2px";
-    button.style.cursor = "pointer";
-    return button;
+const byOpRepliesFirst = (a, b) => {
+  const quoteA = a.querySelector(".postMessage .quotelink")?.innerText || "";
+  const quoteB = b.querySelector(".postMessage .quotelink")?.innerText || "";
+  return isOpReplyRegex.test(quoteB) - isOpReplyRegex.test(quoteA);
+};
+
+const createSortButton = (text, sortMethod) => {
+  let button = document.createElement("div");
+  button.addEventListener("click", () => sortComments(sortMethod));
+  button.innerHTML = text;
+  button.style.backgroundColor = "#444444";
+  button.style.color = "white";
+  button.style.textAlign = "center";
+  button.style.border = "none";
+  button.style.padding = "2px";
+  button.style.cursor = "pointer";
+  return button;
 };
 
 const createBtnContainer = () => {
-    let div = document.createElement("div");
-    div.style.display = "grid";
-    div.style.gridTemplateColumns = "repeat(2, 1fr)";
-    div.style.gridGap = "2px";
-    return div;
+  let div = document.createElement("div");
+  div.style.display = "grid";
+  div.style.gridTemplateColumns = "auto auto auto";
+  div.style.gridGap = "2px";
+  return div;
 };
 
-const onThreadWatcherReady = () => {
-    let threadWatcher = document.getElementById("threadWatcher");
-
-    if (!threadWatcher) return;
-
+const setupUiWhenTWReady = () => {
+  let threadWatcher = document.getElementById("threadWatcher");
+  if (threadWatcher) {
     threadWatcher.appendChild(btnContainer);
     docObserver.disconnect();
+  }
 };
 
 let btnContainer = createBtnContainer();
-btnContainer.appendChild(createSortButton("by replies", () => { sortComments(byRepliesDesc); }));
-btnContainer.appendChild(createSortButton("by time", () => { sortComments(byTimeDesc); }));
-btnContainer.appendChild(createSortButton("images first", () => { sortComments(byImagesFirst); }));
-btnContainer.appendChild(createSortButton("videos first", () => { sortComments(byVideosFirst); }));
-btnContainer.appendChild(createSortButton("links first", () => { sortComments(byLinksFirst); }));
-btnContainer.appendChild(createSortButton("deadlinks first", () => { sortComments(byDeadLinkFirst); }));
+btnContainer.appendChild(createSortButton("by replies", byRepliesDesc));
+btnContainer.appendChild(createSortButton("by time", byTimeDesc));
+btnContainer.appendChild(createSortButton("images first", byImagesFirst));
+btnContainer.appendChild(createSortButton("videos first", byVideosFirst));
+btnContainer.appendChild(createSortButton("links first", byLinksFirst));
+btnContainer.appendChild(createSortButton("deadlinks first", byDeadLinkFirst));
+btnContainer.appendChild(
+  createSortButton("op replies first", byOpRepliesFirst),
+);
 
-const docObserver = new MutationObserver(onThreadWatcherReady);
-docObserver.observe(document, { attributes: false, childList: true, subtree: true });
+let docObserver = new MutationObserver(setupUiWhenTWReady);
+docObserver.observe(document, {
+  attributes: false,
+  childList: true,
+  subtree: true,
+});
